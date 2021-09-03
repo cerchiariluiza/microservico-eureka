ff2c2d83428d14326113ef9d809326563f5705d9
cerchiariluiza <cerchiariluiza>
2021-09-03 17:56:56 -0300

    base

---
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
@@ -0,0 +1,39 @@
+package com.devsuperior.hrpayroll.services;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.client.RestTemplate;
+
+import com.devsuperior.hrpayroll.entities.Payment;
+import com.devsuperior.hrpayroll.entities.Worker;
+import com.devsuperior.hrpayroll.feignclients.WorkerFeignClient;
+
+
+@Service
+public class PaymentService {
+
+//	 //@FeignClient(name = "hr-worker", url = "localhost:8001", path = "/workers")
+//	public interface WorkerFeignClient {
+//
+//		
+//		@GetMapping(value = "/{id}")
+//		ResponseEntity<Worker> findById(@PathVariable Long id);
+//	}
+
+	@Autowired
+	private WorkerFeignClient workerFeignClient;
+	
+	public Payment getPayment(long workerId, int days) {
+		
+		Worker worker = workerFeignClient.findById(workerId).getBody();
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
+	}
+}
---
+++ hr-worker/src/main/java/com/luiza/hrworker/resources/WorkerResource.java
@@ -0,0 +1,46 @@
+package com.luiza.hrworker.resources;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.luiza.hrworker.entitys.Worker;
+
+import com.luiza.hrworker.repositorys.WorkerRespository;
+
+@RestController
+@RequestMapping(value = "/workers")
+public class WorkerResource {
+	
+	
+	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+	
+	@Autowired
+	private Environment env;
+	
+	@Autowired
+	private WorkerRespository repository;
+	
+	
+	@GetMapping
+	public ResponseEntity<List<Worker>>findAll(){
+		List<Worker> list = repository.findAll();
+		return ResponseEntity.ok(list);
+		
+	}
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Worker> findById(@PathVariable Long id){
+		logger.info("PORT = " + env.getProperty("local.server.port"));
+		Worker obj = repository.findById(id).get();
+		return ResponseEntity.ok(obj);
+		
+	}
+
+}
